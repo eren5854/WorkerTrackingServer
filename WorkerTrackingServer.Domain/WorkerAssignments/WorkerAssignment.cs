9cfa9874198e978afb08a498832d6a8a41c893e3
eren5854 <ihsanerendelibas@hotmail.com>
2025-02-22 02:57:28 +0300

    .

--- WorkerTrackingServer.Domain/WorkerAssignments/WorkerAssignment.cs
+++ WorkerTrackingServer.Domain/WorkerAssignments/WorkerAssignment.cs
@@ -1,4 +1,5 @@
-﻿using WorkerTrackingServer.Domain.Abstract;
+﻿using System.Text.Json.Serialization;
+using WorkerTrackingServer.Domain.Abstract;
 using WorkerTrackingServer.Domain.Machines;
 using WorkerTrackingServer.Domain.Products;
 using WorkerTrackingServer.Domain.Users;
@@ -9,13 +10,39 @@ public sealed class WorkerAssignment : Entity
     //public Guid? WorkerId { get; set; }
     //public Worker? Worker { get; set; }
 
+    public object? UserInfo => new
+    {
+        AppUserId = AppUserId,
+        FullName = AppUser.FullName,
+    };
+
+    [JsonIgnore]
     public Guid AppUserId { get; set; } = default!;
+    [JsonIgnore]
     public AppUser AppUser { get; set; } = default!;
 
+    public object? MachineInfo => new
+    {
+        MachineId = MachineId,
+        MachineName = Machine?.MachineName,
+        MachineNumber = Machine?.MachineNumber,
+    };
+
+    [JsonIgnore]
     public Guid? MachineId { get; set; }
+    [JsonIgnore]
     public Machine? Machine { get; set; }
 
+    public object? ProductInfo => new
+    {
+        ProductId = ProductId,
+        ProductName = Product?.ProductName,
+        ProductCode = Product?.ProductCode,
+    };
+
+    [JsonIgnore]
     public Guid? ProductId { get; set; }
+    [JsonIgnore]
     public Product? Product { get; set; }
 
     public int TargetQuantity { get; set; }
